eda8b59320be3972386a4d53c3997a8bf54b5a2a
hnieto <hernan.nieto89@gmail.com>
2021-03-01 21:20:25 -0300

    wip

--- start.sh
+++ start.sh
@@ -17,6 +17,6 @@ echo "0" > /sys/class/gpio/gpio10/value;
 echo "0" > /sys/class/gpio/gpio9/value;
 
 bash -c "source /home/pi/.pyenv/versions/scheduler/bin/activate && python /home/pi/Desktop/pig.scheduler/main.py > /home/pi/Desktop/sch.log 2>&1";
-bash -c "source /home/pi/.pyenv/versions/pig/bin/activate && cd /home/pi/Desktop/pig.station/ && flask run -p 5234 > /home/pi/Desktop/pig.log 2>&1";
+bash -c "source /home/pi/.pyenv/versions/pig/bin/activate && cd /home/pi/Desktop/pig.station/ && flask run > /home/pi/Desktop/pig.log 2>&1";
 bash -c "source /home/pi/.pyenv/versions/hub/bin/activate && cd /home/pi/Desktop/pig.hub/ && flask run -p 5001 -h 0.0.0.0 > /home/pi/Desktop/hub.log 2>&1";
-http get localhost:5234/ping;
+http get localhost:5000/ping;
