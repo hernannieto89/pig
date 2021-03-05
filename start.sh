6550b33700cd90c2f1259140294db522573e491d
hnieto <hernan.nieto89@gmail.com>
2021-03-05 19:41:52 -0300

    wip

--- start.sh
+++ start.sh
@@ -16,12 +16,12 @@ echo "0" > /sys/class/gpio/gpio22/value;
 echo "0" > /sys/class/gpio/gpio10/value;
 echo "0" > /sys/class/gpio/gpio9/value;
 
-bash -c "source /home/pi/.pyenv/versions/scheduler/bin/activate && python /home/pi/Desktop/pig.scheduler/main.py > /home/pi/sch.log 2>&1 &";
+bash -c "source /home/pi/.pyenv/versions/scheduler/bin/activate && python /home/pi/Desktop/pig.scheduler/main.py 2> /home/pi/sch.log &";
 sleep 1;
-bash -c "source /home/pi/.pyenv/versions/pig/bin/activate && cd /home/pi/Desktop/pig.station/ && flask run > /home/pi/pig.log  2>&1 &";
+bash -c "source /home/pi/.pyenv/versions/pig/bin/activate && cd /home/pi/Desktop/pig.station/ && flask run 2> /home/pi/pig.log &";
 sleep 1;
 curl http://localhost:5000/ping/ > /home/pi/test_ping 2>&1;
 sleep 1;
-bash -c "source /home/pi/.pyenv/versions/hub/bin/activate && cd /home/pi/Desktop/pig.hub/ && flask run -p 5001 -h 0.0.0.0 > /home/pi/hub.log  2>&1 &";
+bash -c "source /home/pi/.pyenv/versions/hub/bin/activate && cd /home/pi/Desktop/pig.hub/ && flask run -p 5001 -h 0.0.0.0 2> /home/pi/hub.log &";
 sleep 1;
 curl http://localhost:5000/ping/ > /home/pi/test_ping 2>&1;
