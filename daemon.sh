eddd40ec546e025888602c72423c09b7d59a872e
hnieto <hernan.nieto89@gmail.com>
2021-03-06 20:30:02 -0300

    wip

--- daemon.sh
+++ daemon.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This script copies files from my homedirectory into the webserver directory.
-# (use scp and SSH keys for a remote directory)
-# A new directory is created every hour.
 
 while true; do 
     http get localhost:5000/ping/;
